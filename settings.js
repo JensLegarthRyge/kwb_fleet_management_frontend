0d1cdc8a5a79f811018c943f97664b520fc0c43d
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2022-12-14 10:59:53 +0100

    Updated brand-status text to inform of prior + updated information

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 
-export let SERVER_URL = "http://localhost:8080/api"
-export let AUTH_URL = "http://localhost:8080/api/auth"
+export let SERVER_URL = "https://fleet-management-kwbco.azurewebsites.net/api"
+export let AUTH_URL = "https://fleet-management-kwbco.azurewebsites.net/api/auth"
 
 export let SPECIFIC_CAR_MODEL_URL = "http://localhost:8080/api/specific-car-model"
